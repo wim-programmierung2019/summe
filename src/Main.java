ff4938688030db381122be5c2c065c52dd13fc2c
Christoph Rieger <rieger.chris@googlemail.com>
2019-07-18 17:25:46 +0000

    Summenbilder stub

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    /**
+     * Summenbilder
+     */
+    public static void main(String[] args){
+        // Implementieren Sie ein Programm, dass so lange Eingaben 
+        // annimmt bis eine 0 eingegeben wird. Danach werden alle Zahlen der 
+        // Reihe nach als formatierte Summe ausgegeben, z.B. “3 + 4 + 17 = 24”
+        // Tipp: Die Methode scanner.readInt() liest eine neue Ganzzahl von der Konsole ein
+
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+    
+}
